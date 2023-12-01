27021ccaf4f1391551abe43e9ce058c7cd8474c6
Federico Porpora <porporafederico@gmail.com>
2023-12-01 15:40:00 +0100

    es

--- prova/main.c
+++ prova/main.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(void) {
-	FILE* fp;
-	char s[64];
-	if ((fp = fopen("prova.txt", "w")) == NULL) {
-		exit(1);
-	}
-	do {
-		scanf("%s", s);
-		if (strcmp("fine", s) != 0) {
-			fprintf(fp, "%s ", s);
+#include <stdbool.h>
+
+int main() {
+
+	char ago[10] = "puppa";
+	char pagliaio[50] = "pupupupupupupupupupupupupupupupupupuppa";
+
+	printf("%d", strStr(pagliaio, ago));
+
+	return 0;
+
+}
+
+int strStr(char* haystack, char* needle) {
+	long long i, j = 0;
+	for (i = 0; i <= strlen(haystack); i++) {
+		if (i == strlen(haystack)) {
+			return -1;
 		}
-	} while (strcmp("fine", s) != 0);
-	fclose(fp);
-	return (0);
+		if (haystack[i] == needle[0]) {
+			while (j <= strlen(needle)) {
+				if (j == strlen(needle)) {
+					return i;
+				}
+				if (needle[j] == haystack[i + j]) {
+					j++;
+				}
+				else { break; }
+			}
+		}
+	}
 }
